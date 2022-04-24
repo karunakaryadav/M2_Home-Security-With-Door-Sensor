5610ea3be9eabdee4a9a3a3d4ca78a44cb94e4de
karunakaryadav <101046333+karunakaryadav@users.noreply.github.com>
2022-04-24 23:19:49 +0530

    Add files via upload

---
+++ 3_Implementation/Security.c/main.c
@@ -0,0 +1,30 @@
+/*
+ * Security.c
+ *
+ * Created: 24-04-2022 23:08:54
+ * Author : KARUNAKAR
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+
+{
+	DDRB=DDRB & 0b11111111;//fd Input pin b of controller logic 0 for high
+
+	DDRC=DDRC | 0b01000000;//40 Output pin c of controller logic 1 for low
+
+	while(1)
+
+	{
+		if(PINB & 0b00000010)//02 check door open
+		PORTC=PORTC | 0b01000000;//40 seted high LED
+		else
+		PORTC=PORTC & 0b10111111;//bf seted low LED
+	}
+
+	return 0;
+
+}
+
+
